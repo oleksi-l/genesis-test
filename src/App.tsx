94d4dadc6d4168ede6b980353b67e42df8abcaf6
oleksi-l <oleksi.lomakin@gmail.com>
2023-01-04 01:54:52 +0200

    Added husky to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import FinalScreen from './pages/FinalScreen/FinalScreen';
+// import FinalScreen from './pages/FinalScreen/FinalScreen';
 import GameScreen from './pages/GameScreen/GameScreen';
-import InitialScreen from './pages/InitialScreen/InitialScreen';
+// import InitialScreen from './pages/InitialScreen/InitialScreen';
 
 function App() {
   return (
