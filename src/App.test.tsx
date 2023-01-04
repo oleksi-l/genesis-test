5058dbcf0c6686e9c905590bb5f10231957e7fa8
oleksi-l <oleksi.lomakin@gmail.com>
2023-01-04 02:04:22 +0200

    fix failed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders first screen', () => {
+  render(<App />);
+  const gameScreen = screen.getByText(/Who wants/i);
+  expect(gameScreen).toBeInTheDocument();
+});
